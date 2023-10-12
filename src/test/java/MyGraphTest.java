dc6f0be37f06078f7932d38a794bd0a8f5ffcaf3
vienle <VienDacLe@gmail.com>
2023-10-11 21:40:28 -0700

    Second commit

--- src/test/java/MyGraphTest.java
+++ src/test/java/MyGraphTest.java
@@ -1,18 +1,43 @@
+import org.example.MyGraph;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.Before;
 import org.junit.After;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class MyGraphTest {
     private String filePath = "C:\\Users\\levie\\OneDrive\\Documents\\aFall2023\\CSE464\\Project1";
-    private String inputPath = "C:\\Users\\levie\\OneDrive\\Documents\\aFall2023\\CSE464\\Project1\\input.dot";
+    private String inputPath = "input.dot";
+    private String outputText = "output.txt";
+    private String exptectFile1 = "expected1.txt";
 
+    private MyGraph graph;
     @Before
     public void setUp()
+    {
+        graph = new MyGraph();
+    }
+
+    @Test
+    public void feature1Test() throws IOException {
+        graph.parseGraph(inputPath);
+        graph.outputGraph(outputText);
+        String expected = Files.readString(Paths.get(exptectFile1));
+        String output = Files.readString(Paths.get(outputText));
+        Assert.assertEquals(expected, output);
+    }
+
+    @Test
+    public void feature2Test()
     {
 
     }
 
     @Test
-    public void parseGraphTest()
+    public void feature3Test()
     {
 
     }
@@ -56,6 +81,6 @@ public class MyGraphTest {
     @After
     public void tearDown()
     {
-
+        System.out.println("This is tear down");
     }
 }
