568caac2bb65d99652686bad43e40e5373afcf3d
vienle <dvle3@asu.edu>
2023-11-03 14:52:48 -0700

    Ignore Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,17 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        MyGraph graph = new MyGraph();
-        String filepath = "src\\main\\java\\org\\example\\input.dot";
-        graph.parseGraph(filepath);
-        System.out.println(graph.toString());
-
-        String srcLabel = "a";
-        String dstLabel = "h";
-        String[] nodelist = {"e", "f", "g", "h"};
-        boolean result = graph.addNodes(nodelist);
-        System.out.println(graph.toString());
-        result = graph.addEdge(srcLabel, dstLabel);
-        System.out.println(graph.toString());
+//        MyGraph graph = new MyGraph();
+//        String filepath = "src\\main\\java\\org\\example\\input.dot";
+//        graph.parseGraph(filepath);
+//        System.out.println(graph.toString());
+//
+//        String srcLabel = "a";
+//        String dstLabel = "h";
+//        String[] nodelist = {"e", "f", "g", "h"};
+//        boolean result = graph.addNodes(nodelist);
+//        System.out.println(graph.toString());
+//        result = graph.addEdge(srcLabel, dstLabel);
+//        System.out.println(graph.toString());
     }
 }
